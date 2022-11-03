36d43f360d21664ac6804ec4b48bfa1b5deaeaf7
brunogroth <brunom.groth@gmail.com>
2022-11-02 21:05:55 -0300

    feat: about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/portfolio1.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
     <section id='about'>
-      
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about-container">
+        <div className="about-me">
+          <div className="about-me-image">
+            <img src={ME} alt="About" />
+          </div>
+        </div>
+        <div className="about-content">
+          <div className="about-cards">
+            <article className='about-card'>
+              <FaAward className='about-icon'/>
+              <h5>Experience</h5>
+              <small>1 Year Working</small>
+            </article>
+
+            <article className='about-card'>
+              <FiUsers className='about-icon'/>
+              <h5>Clients</h5>
+              <small>9999 Clients Worldwide</small>
+            </article>
+                    
+            <article className='about-card'>
+              <VscFolderLibrary className='about-icon'/>
+              <h5>Projects</h5>
+              <small>100+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam autem, ipsa aut quis rerum doloremque similique necessitatibus exercitationem at! Nostrum tempora libero eligendi eos aliquid iste quod perferendis molestias ad!
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
     </section>
   )
 }
