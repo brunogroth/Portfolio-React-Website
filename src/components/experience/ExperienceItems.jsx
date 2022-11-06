fa10378a8686c918898a8658acff2fa026e23f1d
brunogroth <brunom.groth@gmail.com>
2022-11-06 17:28:16 -0300

    feat: experience section. fix: index & header CSS.

--- src/components/experience/ExperienceItems.jsx
+++ src/components/experience/ExperienceItems.jsx
@@ -4,9 +4,11 @@ import {BsPatchCheckFill} from 'react-icons/bs'
 const ExperienceItems = ({item, experience}) => {
   return (
     <article className='experience-details'>
-        <BsPatchCheckFill/>
-        <h4>{item}</h4>
-        <small className='text-light'>{experience}</small>
+        <BsPatchCheckFill className='experience-details-icon'/>
+        <div>
+          <h4>{item}</h4>
+          <small className='text-light'>{experience}</small>
+        </div>
   </article>
   )
 }
