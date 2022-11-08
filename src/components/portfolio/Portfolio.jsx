95df7c634ab9c40cb4576df4841312597520ddb5
brunogroth <brunom.groth@gmail.com>
2022-11-07 23:53:31 -0300

    created Portfolio section. First project added.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,82 @@
 import React from 'react'
-import './portfolio.css'
+import './portfolio.scss'
+import igorbarros from '../../assets/igorbarros.webp'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
 const Portfolio = () => {
   return (
-    <div>
-      
-    </div>
+    <section id='portfolio'>
+       <h5>My Recent Work</h5>
+       <h2>Portfolio</h2>
+
+       <div className="container portfolio-container">
+          <article className='portfolio-item'>
+            <div className='portfolio-item-image'>
+              <a href="https://igorbarros.com" target="_blank" rel="noreferrer"><img src={igorbarros} alt="Igor Barros Dealer Cars website"></img></a>
+            </div>
+              <h3><a href="https://igorbarros.com" target="_blank" rel="noreferrer">Igor Barros Website</a></h3>
+              <p> Igor Barros is an Car Dealer at Florida. I developed and performed the complete translation of this website working for <a href="https://autoconf.com.br/" target="_blank" rel="noreferrer">Autoconf</a>.</p>
+              <p>This is a huge step for the company, putting the feet on foreign territory, and also for me, beign able to apply my english skills in hands-on.</p>
+              <div className="portfolio-item-cta">
+                {/* <a href="https://igorbarros.com" className='btn'>Github</a> */}
+                <a href="https://igorbarros.com" target="_blank" rel="noreferrer" className='btn btn-primary'>Visit Website</a>
+              </div>
+          </article>
+          <article className='portfolio-item'>
+            <div className='portfolio-item-image'>
+              <img src={IMG1} alt="Igor Barros Dealer Cars website"></img>
+            </div>
+              <h3>This is a portfolio item title</h3>
+              <div className="portfolio-item-cta">
+                {/* <a href="https://igorbarros.com" className='btn'>Github</a> */}
+                <a href="https://igorbarros.com" className='btn btn-primary'>Visit Website</a>
+              </div>
+          </article>
+          <article className='portfolio-item'>
+            <div className='portfolio-item-image'>
+              <img src={IMG2} alt="Igor Barros Dealer Cars website"></img>
+            </div>
+              <h3>This is a portfolio item title</h3>
+              <div className="portfolio-item-cta">
+                {/* <a href="https://igorbarros.com" className='btn'>Github</a> */}
+                <a href="https://igorbarros.com" className='btn btn-primary'>Visit Website</a>
+              </div>
+          </article>
+          <article className='portfolio-item'>
+            <div className='portfolio-item-image'>
+              <img src={IMG3} alt="Igor Barros Dealer Cars website"></img>
+            </div>
+              <h3>This is a portfolio item title</h3>
+              <div className="portfolio-item-cta">
+                {/* <a href="https://igorbarros.com" className='btn'>Github</a> */}
+                <a href="https://igorbarros.com" className='btn btn-primary'>Visit Website</a>
+              </div>
+          </article>
+          <article className='portfolio-item'>
+            <div className='portfolio-item-image'>
+              <img src={IMG4} alt="Igor Barros Dealer Cars website"></img>
+            </div>
+              <h3>This is a portfolio item title</h3>
+              <div className="portfolio-item-cta">
+                {/* <a href="https://igorbarros.com" className='btn'>Github</a> */}
+                <a href="https://igorbarros.com" className='btn btn-primary'>Visit Website</a>
+              </div>
+          </article>
+          <article className='portfolio-item'>
+            <div className='portfolio-item-image'>
+              <img src={IMG5} alt="Igor Barros Dealer Cars website"></img>
+            </div>
+              <h3>This is a portfolio item title</h3>
+              <div className="portfolio-item-cta">
+                {/* <a href="https://igorbarros.com" className='btn'>Github</a> */}
+                <a href="https://igorbarros.com" className='btn btn-primary'>Visit Website</a>
+              </div>
+          </article>
+       </div>
+    </section>
   )
 }
 
