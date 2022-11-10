a58519253537a1c1ec0ae9c030a5cc125513d45a
Bruno Groth <brunom.groth@gmail.com>
2022-11-10 00:01:40 -0300

    feat: portfolio generated dinamically consuming object, create Testimonial section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,81 +1,87 @@
 import React from 'react'
 import './portfolio.scss'
-import igorbarros from '../../assets/igorbarros.webp'
+import IGORBARROS from '../../assets/igorbarros.webp'
 import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
+
+const projects = [
+  {
+    id: 1,
+    image: IGORBARROS,
+    title: 'Igor Barros Website',
+    github: '',
+    description: "Igor Barros is an brazilian Car Dealer at Florida. I developed the website and performed it's complete translation working for Autoconf. This is a huge step for the company, putting the feet on foreign territory, and also for me, beign able to apply my english skills in a real project.",
+    website: 'https://igorbarros.com'
+  },
+  {
+    id: 2,
+    image: IMG1,
+    title: 'This is a portfolio item title',
+    github: 'https://github.com/brunogroth',
+    description: '',
+    website: 'https://github.com/brunogroth'
+  },
+  {
+    id: 3,
+    image: IMG2,
+    title: 'This is a portfolio item title',
+    github: 'https://github.com/brunogroth',
+    description: '',
+    website: 'https://github.com/brunogroth'
+  },
+  {
+    id: 4,
+    image: IMG3,
+    title: 'This is a portfolio item title',
+    github: 'https://github.com/brunogroth',
+    description: '',
+    website: 'https://github.com/brunogroth'
+  },
+  {
+    id: 5,
+    image: IMG4,
+    title: 'This is a portfolio item title',
+    github: 'https://github.com/brunogroth',
+    description: '',
+    website: 'https://github.com/brunogroth'
+  },
+  {
+    id: 6,
+    image: IMG5,
+    title: 'This is a portfolio item title',
+    github: 'https://github.com/brunogroth',
+    description: '',
+    website: 'https://github.com/brunogroth'
+  }
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-       <h5>My Recent Work</h5>
-       <h2>Portfolio</h2>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-       <div className="container portfolio-container">
-          <article className='portfolio-item'>
-            <div className='portfolio-item-image'>
-              <a href="https://igorbarros.com" target="_blank" rel="noreferrer"><img src={igorbarros} alt="Igor Barros Dealer Cars website"></img></a>
-            </div>
-              <h3><a href="https://igorbarros.com" target="_blank" rel="noreferrer">Igor Barros Website</a></h3>
-              <p> Igor Barros is  an brazilian Car Dealer at Florida. I developed the website and performed it's complete translation working for <a href="https://autoconf.com.br/" target="_blank" rel="noreferrer">Autoconf</a>.</p>
-              <p>This is a huge step for the company, putting the feet on foreign territory, and also for me, beign able to apply my english skills in a real project.</p>
-              <div className="portfolio-item-cta">
-                {/* <a href="https://igorbarros.com" className='btn'>Github</a> */}
-                <a href="https://igorbarros.com" target="_blank" rel="noreferrer" className='btn btn-primary'>Visit Website</a>
+      <div className="container portfolio-container">
+        {
+          projects.map(({ id, image, title, description, github, website }) => {
+            return (<article key={id} className='portfolio-item'>
+              <div className='portfolio-item-image'>
+                <a href={website} target="_blank" rel="noreferrer"><img src={image} alt={title}></img></a>
               </div>
-          </article>
-          <article className='portfolio-item'>
-            <div className='portfolio-item-image'>
-              <img src={IMG1} alt="Igor Barros Dealer Cars website"></img>
-            </div>
-              <h3>This is a portfolio item title</h3>
+              <h3><a href={website} target="_blank" rel="noreferrer">{title}</a></h3>
+              <p>{description}</p>
               <div className="portfolio-item-cta">
-                {/* <a href="https://igorbarros.com" className='btn'>Github</a> */}
-                <a href="https://igorbarros.com" className='btn btn-primary'>Visit Website</a>
+                {github && <a href={github} className='btn'>Github</a>} {/* ternary IF with only true condition */}
+                <a href={website} target="_blank" rel="noreferrer" className='btn btn-primary'>Visit Website</a>
               </div>
-          </article>
-          <article className='portfolio-item'>
-            <div className='portfolio-item-image'>
-              <img src={IMG2} alt="Igor Barros Dealer Cars website"></img>
-            </div>
-              <h3>This is a portfolio item title</h3>
-              <div className="portfolio-item-cta">
-                {/* <a href="https://igorbarros.com" className='btn'>Github</a> */}
-                <a href="https://igorbarros.com" className='btn btn-primary'>Visit Website</a>
-              </div>
-          </article>
-          <article className='portfolio-item'>
-            <div className='portfolio-item-image'>
-              <img src={IMG3} alt="Igor Barros Dealer Cars website"></img>
-            </div>
-              <h3>This is a portfolio item title</h3>
-              <div className="portfolio-item-cta">
-                {/* <a href="https://igorbarros.com" className='btn'>Github</a> */}
-                <a href="https://igorbarros.com" className='btn btn-primary'>Visit Website</a>
-              </div>
-          </article>
-          <article className='portfolio-item'>
-            <div className='portfolio-item-image'>
-              <img src={IMG4} alt="Igor Barros Dealer Cars website"></img>
-            </div>
-              <h3>This is a portfolio item title</h3>
-              <div className="portfolio-item-cta">
-                {/* <a href="https://igorbarros.com" className='btn'>Github</a> */}
-                <a href="https://igorbarros.com" className='btn btn-primary'>Visit Website</a>
-              </div>
-          </article>
-          <article className='portfolio-item'>
-            <div className='portfolio-item-image'>
-              <img src={IMG5} alt="Igor Barros Dealer Cars website"></img>
-            </div>
-              <h3>This is a portfolio item title</h3>
-              <div className="portfolio-item-cta">
-                {/* <a href="https://igorbarros.com" className='btn'>Github</a> */}
-                <a href="https://igorbarros.com" className='btn btn-primary'>Visit Website</a>
-              </div>
-          </article>
-       </div>
+            </article>
+            )
+          })
+        }
+      </div>
     </section>
   )
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './testimonials.scss'
+
+import { IconBase } from 'react-icons'
+import { FaUser } from 'react-icons/fa'
 const Testimonials = () => {
   return (
-    <div>
-      
-    </div>
+    <section id='testimonials'>
+      <h5>Review from known</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials-container">
+        <article className="testimonial">
+          <div className="client-avatar">
+            <FaUser/>
+            <h5 className='client-name'> Name 01 </h5>
+            <small className='client-review'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem illum libero blanditiis quis asperiores, beatae veniam reiciendis et magnam quod dolore voluptatibus velit molestiae sit. Odio nisi sunt ab laboriosam?
+            </small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
